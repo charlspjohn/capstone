1bf52d4f195cd967e2432ed44ce8cabca8cfbb1c
Charls P John <charlspjohn@gmail.com>
2020-06-13 09:35:26 +0530

    Add files via upload

---
+++ startup-script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Start minikube
+sudo minikube start --driver=none
+
+# Change owneship of kubeconfig
+sudo chown -R $USER $HOME/.kube $HOME/.minikube
+
+# Enable ingress controller
+sudo minikube addons enable ingress
+
+# deploy helmchart of kubernetes-dashboard
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm repo update
+helm upgrade --install k8s-dashbaord kubernetes-dashboard/kubernetes-dashboard --namespace  kube-system --values kubernetes-dashboard-values.yaml
+
+# create devops namespace
+kubectl create ns devops
+
+# deploy jenkins
+helm repo add stable https://kubernetes-charts.storage.googleapis.com
+helm repo update
+helm upgrade --install jenkins stable/jenkins --namespace devops --values jenkins-values.yaml
