2c024ac379c5ae18d67467fe00b010754144f730
root <root@minikubevm.capstone.com>
2020-08-02 08:54:39 +0000

    mounted docker sock

--- startup-script.sh
+++ startup-script.sh
@@ -20,6 +20,12 @@ else
 	sudo chmod 777 /tmp/hostpath-provisioner -R
 fi
 
+# Fix the docker socket permissions
+SOCK_PERM=$(echo $(stat -c '%A' "/var/run/docker.sock"))
+if [[ "$SOCK_PERM" != "srw-rw-rw-" ]]; then
+	chmod 666 /var/run/docker.sock
+fi
+
 # Start minikube
 sudo minikube start --driver=none
 
