75fb244792ae88b339f990b57241c4ba0ece0d76
root <root@minikubevm.capstone.com>
2020-08-02 13:58:06 +0000

    modified startup script

--- startup-script.sh
+++ startup-script.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+export GITLABPASSWORD="$(head -c 512 /dev/urandom | LC_CTYPE=C tr -cd 'a-zA-Z0-9' | head -c 32)"
+export GITLABREGISTRY="https://registry.capstone.com/v2/"
+export GITLABUSER="root"
+
 function create_namespace() {
 	name=$1
-	if ! kubectl get ns $name; then
+	if ! kubectl get ns $name >/dev/null 2>&1; then
 		kubectl create ns $name
 		kubectl apply -f secrets/secret-tls-cert.yaml -n $name
 		kubectl apply -f secrets/secret-self-signed-ca.yaml -n $name
+		kubectl create secret docker-registry regcred \
+		--docker-server=${GITLABREGISTRY} \
+		--docker-username=${GITLABUSER} \
+		--docker-password=${GITLABPASSWORD} \
+		--docker-email=test@capstone.com -n $name
 	fi
 }
 
@@ -13,6 +22,9 @@ function create_namespace() {
 CHK_FSTAB=$(grep '/tmp/hostpath-provisioner' /etc/fstab | wc -l)
 if [[ "$CHK_FSTAB" == "0" ]]; then
 	sudo echo '/var/tmp/hostpath-provisioner /tmp/hostpath-provisioner none defaults,bind 0 0' >> /etc/fstab
+	echo "adding root ca of capstone.com to system"
+	sudo cp tls/rootCA.crt /usr/local/share/ca-certificates/
+	sudo update-ca-certificates
 	echo "Need a system reboot to configure new hostpath for minikube volumes. Rebooting the system in 10 seconds.."
 	sleep 10
 	sudo shutdown -r now
@@ -48,7 +60,7 @@ helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm repo add gitlab https://charts.gitlab.io/
 helm repo update
 
-# create tls secrets
+# create tls secrets in system namespaces
 kubectl apply -f secrets/secret-tls-cert.yaml -n default
 kubectl apply -f secrets/secret-tls-cert.yaml -n kube-system
 
@@ -58,11 +70,14 @@ helm upgrade --install k8s-dashbaord kubernetes-dashboard/kubernetes-dashboard -
 # create devops namespace
 create_namespace devops
 
+#create namespace to deploy apps
+create_namespace app
+
 # deploy jenkins
 helm upgrade --install jenkins stable/jenkins --namespace devops --values jenkins-values.yaml --version 2.4.1
 
 # deploy gitlab
-kubectl create secret generic gitlab-gitlab-initial-root-password --from-literal=username=root --from-literal=password=$(head -c 512 /dev/urandom | LC_CTYPE=C tr -cd 'a-zA-Z0-9' | head -c 32) -n devops
+kubectl create secret generic gitlab-gitlab-initial-root-password --from-literal=username=${GITLABUSER} --from-literal=password=${GITLABPASSWORD} -n devops
 kubectl apply -f secrets/secret-gitlab-gitlab-shell-host-keys.yaml -n devops
 helm upgrade --install gitlab gitlab/gitlab --namespace devops --values gitlab-values.yaml --version 4.2.0
 
